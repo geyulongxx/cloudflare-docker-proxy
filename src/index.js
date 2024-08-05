b4077585e8adc404837b8a9fc04ababa5a8ab59e
Ge Yulong <247920193@qq.com>
2024-08-05 22:51:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.28256.fun": dockerHub,
+  "quay.28256.fun": "https://quay.io",
+  "gcr.28256.fun": "https://gcr.io",
+  "k8s-gcr.28256.fun": "https://k8s.gcr.io",
+  "k8s.28256.fun": "https://registry.k8s.io",
+  "ghcr.28256.fun": "https://ghcr.io",
+  "cloudsmith.28256.fun": "https://docker.cloudsmith.io",
+  "ecr.28256.fun": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.28256.fun": dockerHub,
 };
 
 function routeByHosts(host) {
